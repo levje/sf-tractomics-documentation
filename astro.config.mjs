4a0e62526d27d89c981602c19d02d091a5895a73
Jeremi Levesque <jeremilevesque@hotmail.com>
2026-02-25 20:13:46 -0500

    First cleanups

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import Icons from 'unplugin-icons/vite';
 import starlightVersions from 'starlight-versions';
+import starlightThemeGalaxy from 'starlight-theme-galaxy';
 
 import tailwindcss from '@tailwindcss/vite';
 
@@ -14,9 +15,7 @@ export default defineConfig({
 		starlight({
 			title: 'sf-tractomics',
 			plugins: [
-				// starlightVersions({
-				// 	versions: [ ],
-				// }),
+				starlightThemeGalaxy()
 			],
 			logo: {
 				light: './src/assets/logo_white.png',
@@ -26,16 +25,23 @@ export default defineConfig({
 			social: [{ icon: 'github', label: 'sf-tractomics', href: 'https://github.com/scilus/sf-tractomics' }],
 			sidebar: [
 				{
-					label: 'Guides',
+					label: 'User Guides',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'How to run the pipeline?', slug: 'guides/installation' },
-						{ label: 'Inputs', slug: 'guides/inputs' },
-						{ label: 'Running the pipeline', slug: 'guides/usage' },
-						{ label: 'Running with no internet access', slug: 'guides/nointernet' },
-						{ label: "Parameters", slug: 'guides/parameters' },
-						{ label: 'Outputs', slug: 'guides/outputs' },
-						{ label: 'Frequently Asked Questions', slug: 'guides/faq' },
+						{ label: 'Installation', slug: 'user_guides/installation' },
+						{ label: 'Prepare your input data', slug: 'user_guides/inputs' },
+						{ label: 'Running the pipeline', slug: 'user_guides/usage' },
+						{ label: "Configure the pipeline", slug: 'user_guides/parameters' },
+						{ label: 'Understand the outputs', slug: 'user_guides/outputs' },
+						{ label: 'Frequently Asked Questions', slug: 'user_guides/faq' },
+						{ label: 'Troubleshooting', slug: 'user_guides/troubleshooting' },
+					],
+				},
+				{
+					label: 'Developer Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Setup your development environment', slug: 'dev_guides/setup' },
 					],
 				},
 				{
